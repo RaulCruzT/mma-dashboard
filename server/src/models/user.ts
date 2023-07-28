fb58dae76cbcf8ec1600400a0b289e9adc7f683f
RaulCruzT <cruztorresraul@gmail.com>
2023-07-27 21:34:23 -0400

    user model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,9 +1,10 @@
 import { Schema, InferSchemaType, model } from 'mongoose';
 
 const userSchema = new Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true, unique: true, select: false, match: [/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, 'Please enter a valid email address.'] },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    avatar: { type: String, required: true },
+    allProperties: [{ type: Schema.Types.ObjectId, ref: "Property" }],
     role: { type: Number, required: true, default: 0, enum: [0, 1, 2]},
 }, { timestamps: true });
 
