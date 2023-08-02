ad6a130fbea55942a6262698f6f0f1a4a3fa36b8
RaulCruzT <cruztorresraul@gmail.com>
2023-08-02 19:39:37 -0400

    requiresAuth

--- client/src/interfaces/google.d.ts
+++ client/src/interfaces/google.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface IdConfiguration {
   client_id: string;
   auto_select?: boolean;
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as env } from "./validateEnv";
 export { default as assertIsDefined } from "./assertIsDefined";
+export { default as getToken } from "./getToken";
