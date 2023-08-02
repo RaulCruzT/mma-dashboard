a1bff3865c5dade8e274711cf957fbaf3c07db77
RaulCruzT <cruztorresraul@gmail.com>
2023-08-01 22:33:35 -0400

    minor changes

--- server/src/utils/validateEnv.ts
+++ server/src/utils/validateEnv.ts
@@ -1,7 +1,9 @@
 import { cleanEnv } from 'envalid';
-import { port, str, num, email, url } from 'envalid/dist/validators';
+import { port, str } from 'envalid/dist/validators';
 
 export default cleanEnv(process.env, {
     MONGO_CONNECTION_STRING: str(),
     PORT: port(),
+    GOOGLE_OAUTH_CLIENT_ID: str(),
+    GOOGLE_OAUTH_CLIENT_SECRET: str()
 })
