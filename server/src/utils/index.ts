b937120f24d4875f7798dc3e067a492beedc26ac
RaulCruzT <cruztorresraul@gmail.com>
2023-08-02 21:18:59 -0400

    parseJwt

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as env } from "./validateEnv";
 export { default as assertIsDefined } from "./assertIsDefined";
 export { default as getToken } from "./getToken";
+export { default as parseJwt } from "./parseJwt";
