fe2ded7c44b29d3f0fd030a8e1df95fa3893919f
RaulCruzT <cruztorresraul@gmail.com>
2023-08-07 21:08:51 -0400

    genera routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,7 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import createHttpError, { isHttpError } from "http-errors";
-import { authRoutes, usersRoutes } from "./routes";
+import { authRoutes, generaRoutes, usersRoutes } from "./routes";
 import { requiresAuth } from "./middleware/auth";
 
 const app = express();
@@ -21,6 +21,7 @@ app.use(express.json());
 // Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", requiresAuth, usersRoutes);
+app.use("/api/v1/genera", requiresAuth, generaRoutes);
 
 app.use((req, res, next) => {
     next(createHttpError(404, "Endpoint not found"));
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,2 +1,3 @@
-export { default as usersRoutes } from "./user.routes";
 export { default as authRoutes } from "./auth.routes";
+export { default as usersRoutes } from "./user.routes";
+export { default as generaRoutes } from "./genera.routes";
