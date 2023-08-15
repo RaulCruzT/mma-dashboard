ded772fdf2d420f31b3180d03bbb81d57b994ffe
RaulCruzT <cruztorresraul@gmail.com>
2023-08-14 20:10:14 -0400

    Added assembly and dataprocessed models

---
+++ server/src/models/assembly.ts
@@ -0,0 +1,21 @@
+import { Schema, InferSchemaType, model } from 'mongoose';
+
+const URL = [/^(http|https):\/\/[^ "]+$/, 'Please enter a valid URL.'];
+
+const assemblySchema = new Schema({
+    name: { type: String, required: true, unique: true, minLength: 1 },
+    creator: { type: Schema.Types.ObjectId, ref: "User", select: false },
+    actinobacteria: { type: Schema.Types.ObjectId, ref: "Actinobacteria", select: false },
+    date: { type: Date, required: true },
+    bgcs: { type: String, match: URL },
+    softwareTrimming: { type: String, required: true },
+    softwareAssembly: { type: String, required: true },
+    parametersAssembly: { type: String, required: true },
+    qualityFinal: { type: String, required: true },
+    comments: { type: String },
+    link: { type: String, match: URL }
+}, { timestamps: true });
+
+type Assembly = InferSchemaType<typeof assemblySchema>;
+
+export default model<Assembly>("Assembly", assemblySchema);
---
+++ server/src/models/dataprocessed.ts
@@ -0,0 +1,20 @@
+import { Schema, InferSchemaType, model } from 'mongoose';
+
+const processedDataSchema = new Schema({
+    creator: { type: Schema.Types.ObjectId, ref: "User", select: false },
+    actinobacteria: { type: Schema.Types.ObjectId, ref: "Actinobacteria", select: false },
+    massDetection: {type: String, required: true },
+    chromatogramBuilder: {type: String, required: true },
+    deconvolution: {type: String, required: true },
+    isotope: {type: String, required: true },
+    filtered: {type: String, required: true },
+    identification: {type: String },
+    alignment: {type: String },
+    gapFilling: {type: String },
+    comments: {type: String }
+
+}, { timestamps: true });
+
+type ProcessedData = InferSchemaType<typeof processedDataSchema>;
+
+export default model<ProcessedData>("ProcessedData", processedDataSchema);
