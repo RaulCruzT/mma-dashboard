d29749b51c75fbfd749bed5e0cbe611cab5d8adc
RaulCruzT <cruztorresraul@gmail.com>
2023-08-20 16:18:24 -0400

    updated models

--- server/src/models/assembly.ts
+++ server/src/models/assembly.ts
@@ -3,7 +3,7 @@ import { Schema, InferSchemaType, model } from 'mongoose';
 const URL = [/^(http|https):\/\/[^ "]+$/, 'Please enter a valid URL.'];
 
 const assemblySchema = new Schema({
-    name: { type: String, required: true, unique: true, minLength: 1 },
+    name: { type: String, required: true, unique: true },
     creator: { type: Schema.Types.ObjectId, ref: "User", select: false },
     actinobacteria: { type: Schema.Types.ObjectId, ref: "Actinobacteria", select: false },
     date: { type: Date, required: true },
--- server/src/models/genera.ts
+++ server/src/models/genera.ts
@@ -1,7 +1,7 @@
 import { Schema, InferSchemaType, model } from 'mongoose';
 
 const generaSchema = new Schema({
-    name: { type: String, required: true, unique: true, minLength: 1 },
+    name: { type: String, required: true, unique: true },
     creator: { type: Schema.Types.ObjectId, ref: "User", select: false },
 }, { timestamps: true });
 
