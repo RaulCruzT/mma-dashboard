b595a85215005659f88f8a0e7a4a170f706be393
RaulCruzT <cruztorresraul@gmail.com>
2023-08-20 21:05:53 -0400

    actinobacteria list

--- client/src/pages/actinobacteria/list.tsx
+++ client/src/pages/actinobacteria/list.tsx
@@ -1,7 +1,94 @@
+import { List, useDataGrid, EditButton, DeleteButton } from "@refinedev/mui";
+import { IActinobacteria } from "../../interfaces/actinobacteria";
+import { IResourceComponentsProps } from "@refinedev/core";
+import { DataGrid, GridColDef, GridToolbar, getGridStringOperators } from "@mui/x-data-grid";
+import React from "react";
 
+export const ActinobacteriaList: React.FC<IResourceComponentsProps> = () => {
+    const { dataGridProps } = useDataGrid<IActinobacteria>({
+        initialPageSize: 10,
+    });
 
-export const ActinobacteriaList = () => {
+    const columns = React.useMemo<GridColDef<IActinobacteria>[]>(
+        () => [
+            {
+                field: "identifierStrain",
+                headerName: "Strain",
+                minWidth: 150,
+                flex: 1,
+                filterOperators: getGridStringOperators().filter(
+                    (operator) => operator.value === 'contains'
+                )
+            },
+            {
+                field: "identifierGenera.name",
+                headerName: "Genera",
+                flex: 1,
+                sortable: false,
+                filterable: false,
+            },
+            {
+                field: "identifierSpecies",
+                headerName: "Species",
+                flex: 1,
+                filterOperators: getGridStringOperators().filter(
+                    (operator) => operator.value === 'contains'
+                )
+            },
+            {
+                field: "arnr16sCompleteness",
+                headerName: "16S rRNA",
+                flex: 1,
+                filterOperators: getGridStringOperators().filter(
+                    (operator) => operator.value === 'contains'
+                )
+            },
+            {
+                field: "actions",
+                headerName: "Actions",
+                renderCell: function render({ row }) {
+                    return (
+                        <>
+                            <EditButton
+                                size="small"
+                                hideText
+                                recordItemId={row._id}
+                            />
+                            <DeleteButton
+                                size="small"
+                                hideText
+                                resource="actinobacteria"
+                                recordItemId={row._id}
+                                mutationMode="undoable"
+                                confirmTitle={`Are you sure to delete ${row.identifierStrain} actinobacteria?`}
+                            />
+                        </>
+                    );
+                },
+                align: "center",
+                headerAlign: "center",
+                flex: 0.5,
+                minWidth: 80,
+                sortable: false,
+                filterable: false,
+            },
+        ],
+        [],
+    );
+
     return (
-        <div>ActinobacteriaList</div>
+        <List wrapperProps={{ sx: { paddingX: { xs: 2, md: 0 } } }}>
+            <DataGrid
+                {...dataGridProps}
+                columns={columns}
+                getRowId={(row: IActinobacteria) =>  row._id}
+                filterModel={undefined}
+                autoHeight
+                pageSizeOptions={[10, 20, 50, 100]}
+                slots={{
+                    toolbar: GridToolbar,
+                }}
+            />
+        </List>
     )
 }
