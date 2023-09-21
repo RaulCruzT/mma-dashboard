804d5f3e286e5ca5f05f1178e786de543aeb9e53
RaulCruzT <cruztorresraul@gmail.com>
2023-09-21 20:54:19 -0300

    processed data interface

---
+++ client/src/interfaces/processeddata.d.ts
@@ -0,0 +1,17 @@
+import { IMyActinobacteria } from "./myactinobacteria";
+import { IUser } from "./user";
+
+export interface IAssembly {
+    _id: string;
+    creator: IUser
+    actinobacteria: IMyActinobacteria;
+    massDetection: string;
+    chromatogramBuilder: string;
+    deconvolution: string;
+    isotope: string;
+    filtered: string;
+    identification: string;
+    alignment: string;
+    gapFilling: string;
+    comments: string
+}
